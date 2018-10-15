8ccfe0b92390e22a71acee69a1fca650a32fb6ce
witusj <wfth69@gmail.com>
2018-10-15 22:08:34 +0200

    Update

--- samples/xml2json.js
+++ samples/xml2json.js
@@ -1,20 +1,20 @@
 var fs = require('fs');
 var convert = require('xml-js');
-var xml = require('fs').readFileSync('klanten.xml', 'utf8');
+var xml = require('fs').readFileSync('kavels.xml', 'utf8');
 var options = {
                 compact: true,
                 ignoreAtrributes: true,
                 ignoreDeclaration: true
               };
 var result = convert.xml2js(xml, options);
-var klanten = JSON.stringify(result, null, 2);
+var kavels = JSON.stringify(result, null, 2);
 
 console.log(JSON.stringify(result, null, 2));
 
-fs.writeFile('klanten.json', klanten, 'utf8', function(err){
+fs.writeFile('kavels.json', kavels, 'utf8', function(err){
         if(err){
               console.log(err);
         } else {
               console.log('Everything went OK!');
         }
-      });
+});
